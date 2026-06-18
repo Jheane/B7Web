60e3e93f7b8ffb76d5cda5e7b120698f19df6e4b
Jheane Santos <31616694+Jheane@users.noreply.github.com>
2026-06-18 17:53:59 +0000

    Exercício de fixação de variáveis

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,7 +1,6 @@
-let nome = "Jheane"//string
-let idade = 31//number (int, double)
-let logado = false //booleano
-let lista = ["ovo", "leite", "trigo", "chocolate"] //array, sendo possível buscar por informações através do índice do elemento
-let nomeCompleto = {nome: "Jheane", idade: 31} //objeto com duas informações, sendo possível filtrar pelo objeto através do nome da variável
-console.log(nomeCompleto)//printa o objeto, tendo as informações dele juntamente com o tipo de propriedade do objeto
-console.log(typeof lista)//o typeof mostra o tipo do dado armazenado naquela variável
+let carro = "Ferrari"
+console.log(carro)
+let valorBolo = 19.99
+console.log(valorBolo)
+let cidade = "São Paulo"
+console.log(cidade)
