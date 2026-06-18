b17f1cbd58c504a2b1110034c3726b27751c8d68
Jheane Santos <31616694+Jheane@users.noreply.github.com>
2026-06-18 15:07:38 +0000

    Variáveis, hello world e exibindo popup (alert) e informações no console (console.log)

---
+++ Web/FrontEnd/JavaScript/script.js
@@ -0,0 +1,9 @@
+alert("Olá mundo!") //Gera um popup na tela com a frase informada
+console.log("Olá mundo! Bão?")//Exibe essa informação dentro do console do navegador, não sendo exibida ao usuário
+let idade = 31 //cria a variável idade, recebendo 31 como valor
+let nome = "Jheane" //cria a variável nome recebendo o texto Jheane. Os textos devem ser inseridos entre aspas
+let cidade = "Betim"
+let estado = "Minas Gerais"
+console.log(nome, idade) //Mostra no console as variáveis
+console.log(cidade)
+console.log(estado)//dessa forma, cada informação será exibida em uma linha, diferente da anterior, que mostrou uma informação do lado da outra, na mesma linha
