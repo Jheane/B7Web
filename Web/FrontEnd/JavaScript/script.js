a1e4079c6ce611b8a0572b58ca8753f186fc9b20
Jheane Santos <31616694+Jheane@users.noreply.github.com>
2026-06-19 01:06:18 +0000

    Operações em variáveis

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,10 +1,10 @@
-let nome = "Jheane";//variável let, contém escopo de bloco
-//a variável let não pode ser redeclarada. Pode ser alterada, mas sem chamar novamente pela mesma variável, pois o código não será executado
-console.log(nome);
-nome = "João";
-console.log(nome);
-var nome1 = "Ana";//variável var, contém escopo de função
-console.log(nome1);
-const nome2 = "Bianca";//constante const
-console.log(nome2);
-nome2 = "Bia";//vai dar erro por estarmos tentando atribuir outro valor a uma constante: "Uncaught TypeError: Assignment to constant variable."
+let x = 3//Declaração de variável tipo number, onde será possível executar operações matemáticas normalmente
+//let x = "0";//Aqui o x foi declarado como string, então, caso tente fazer alguma operação de adição, como exemplo abaixo, será feita a concatenação da variável com o 7, invés de somar
+x = x / 7;
+console.log(x);
+
+
+let nome = "Jheane";
+let sobrenome = "Silva";
+let nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
