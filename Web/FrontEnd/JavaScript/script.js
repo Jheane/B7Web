6d4098eb5508b4179140e48c97aa4a1554c17433
Jheane Santos <31616694+Jheane@users.noreply.github.com>
2026-06-19 00:49:08 +0000

    Uso do let, const e var

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,6 +1,10 @@
-let name = "Jheane"; //o ; é opcional
-let idade = 31;
-
+let nome = "Jheane";//variável let, contém escopo de bloco
+//a variável let não pode ser redeclarada. Pode ser alterada, mas sem chamar novamente pela mesma variável, pois o código não será executado
 console.log(nome);
-console.log(idade);
-//o ; é obrigatório quando queremos colocar mais de um código na mesma linha 
+nome = "João";
+console.log(nome);
+var nome1 = "Ana";//variável var, contém escopo de função
+console.log(nome1);
+const nome2 = "Bianca";//constante const
+console.log(nome2);
+nome2 = "Bia";//vai dar erro por estarmos tentando atribuir outro valor a uma constante: "Uncaught TypeError: Assignment to constant variable."
