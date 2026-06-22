653a742bf7bc515e032aa6b794ac3c886e252f47
Jheane Santos <31616694+Jheane@users.noreply.github.com>
2026-06-22 23:05:59 +0000

    Condicional == e ===

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,13 +1,12 @@
-let idade = 18;
-if (idade > 17) {
-    console.log("Você é maior de idade.")
+let idade = "20";
+if(idade === 20){
+    console.log("Você tem 20 anos")
+    console.log(typeof(idade))
+}else{
+    console.log(typeof(idade))
 }
-else{
-    console.log("Você é menor de idade.")
-}
-// <
-// >
-// <=
-// >=
-// == igual
-// !=
+//No exemplo acima, será exibido o string do typeof dentro do else, porque o tipo de dado da variável idade é diferente do tipo de dado do meu if
+
+// == : faz a comparação do valor de idade com o valor do if
+// === : faz a comparação do valor e do tipo com o valor do if
+// Par ao caso do diferente, usamos !==
