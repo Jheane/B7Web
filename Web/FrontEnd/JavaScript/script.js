ba06fc4a06a80060625cfe23e68be71e106e4751
Jheane Santos <31616694+Jheane@users.noreply.github.com>
2026-06-22 22:12:06 +0000

    Condicional If/Else

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,7 +1,13 @@
-let nome = "Jheane";
-let sobrenome = "Santos";
-let idade = 26;
-//let nomeCompleto = nome + ' ' + sobrenome; //forma que usamos anteriormente para concatenar as strings
-//let nomeCompleto = `Jheane Silva Santos`;//Template String: string entre `, onde podemos colocar conjunto de strings. Ele também permite expressões, sendo inseridas entre chaves e precedidas de $
-let nomeCompleto = `Nome: ${nome} ${sobrenome}, idade: ${idade + 5} anos`
-console.log(nomeCompleto);
+let idade = 18;
+if (idade > 17) {
+    console.log("Você é maior de idade.")
+}
+else{
+    console.log("Você é menor de idade.")
+}
+// <
+// >
+// <=
+// >=
+// == igual
+// !=
