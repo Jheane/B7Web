0391d269a35d582deebeed12023551ffc9d4dc67
Jheane Santos <31616694+Jheane@users.noreply.github.com>
2026-06-23 16:40:02 +0000

    Condicional dupla (if else)

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,27 +1,24 @@
 let idade = 45;
-if(idade >= 0 && idade < 120){
-    if(idade >=60 && idade <= 120){
-        console.log("Você é um idoso");
-    }
-    if(idade >= 18 && idade < 60){
-        console.log("Você é um adulto.");
-    }
-    if(idade >= 0 && idade < 18){
-        console.log("Você é uma criança.");
-    }
+if(idade >=60 && idade <= 120){
+    console.log("Você é um idoso");
 }
+else if(idade >= 18 && idade < 60){
+    console.log("Você é um adulto.");
+}
+else if(idade >= 0 && idade < 18){
+    console.log("Você é uma criança.");
+}
+
 else{
     console.log("Digite uma idade válida.");
 }
 
-let sexo = "M";
-if(sexo == "M" || sexo == "F"){
-    if(sexo == "M"){
-        console.log("Você é um homem.");
-    }
-    if(sexo == "F"){
-        console.log("Você é uma mulher.")
-    }
+let sexo = "m";
+if(sexo == "M" || sexo == "m"){
+    console.log("Você é um homem.");
+}
+else if(sexo == "F" || sexo == "f"){
+    console.log("Você é uma mulher.")
 }
 else{
     console.log("Você é de outro gênero.")
