16b4720288a6ab03a9711555cdb66fa34fb54c01
Jheane Santos <31616694+Jheane@users.noreply.github.com>
2026-06-23 00:12:29 +0000

    Multi-condicionais (&& e ||)

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,12 +1,29 @@
-let idade = "20";
-if(idade === 20){
-    console.log("Você tem 20 anos")
-    console.log(typeof(idade))
-}else{
-    console.log(typeof(idade))
+let idade = 45;
+if(idade >= 0 && idade < 120){
+    if(idade >=60 && idade <= 120){
+        console.log("Você é um idoso");
+    }
+    if(idade >= 18 && idade < 60){
+        console.log("Você é um adulto.");
+    }
+    if(idade >= 0 && idade < 18){
+        console.log("Você é uma criança.");
+    }
 }
-//No exemplo acima, será exibido o string do typeof dentro do else, porque o tipo de dado da variável idade é diferente do tipo de dado do meu if
+else{
+    console.log("Digite uma idade válida.");
+}
 
-// == : faz a comparação do valor de idade com o valor do if
-// === : faz a comparação do valor e do tipo com o valor do if
-// Par ao caso do diferente, usamos !==
+let sexo = "M";
+if(sexo == "M" || sexo == "F"){
+    if(sexo == "M"){
+        console.log("Você é um homem.");
+    }
+    if(sexo == "F"){
+        console.log("Você é uma mulher.")
+    }
+}
+else{
+    console.log("Você é de outro gênero.")
+}
+//Uso do && (E) e || (OU)
