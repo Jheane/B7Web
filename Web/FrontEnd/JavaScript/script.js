547a82b8b67305a46f22b8e5c35bd87b43fedf6f
Jheane Santos <31616694+Jheane@users.noreply.github.com>
2026-06-23 17:24:04 +0000

    Condicional ternário

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,21 +1,9 @@
-//Substitua o @ pela condicional correta para que o resultado seja TRUE
-let x = 10;
-let y = 5;
-console.log(x > y);
-//Substitua o @ pela condicional correta para que o resultado seja FALSE
-let w = "10";
-let z = 10;
-console.log(w === z);
-//Substitua o @ pela condicional correta para que o resultado seja TRUE
-let a = 10;
-let b = 5;
-console.log(a != b);
-//Crie uma condicional para verificar se o preço da carne está barato ou caro. PS.: até 45 está barato.
-let preco = 40.3;
-if(preco <= 45){
-    console.log("Tá barato");
-}
-else{
-    console.log("Tá caro.")
-}
-//Exercício de fixação de condicionais
+let isMember = true;
+let age = 45;
+
+let shipping = isMember ? 2 : 10;//shipping vai receber: isMember ? > if; 2 > condição se true; : > else; 10 > condição se false.
+let isAdult = (age >= 18 && age < 60) ? "Sim" : "Não"; 
+console.log(isMember ? "Você é membro" : "Você não é membro.")
+console.log("Valor do frete: R$ " + shipping);
+console.log(isAdult);
+//Condicional ternário: IF em uma linha só
