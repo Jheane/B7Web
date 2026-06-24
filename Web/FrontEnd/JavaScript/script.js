bf7f240bf962ba7f8039717b9f0a93d712f5eac6
Jheane <jheane.s.santos@gmail.com>
2026-06-24 14:29:46 -0300

    Retorno da função

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,11 +1,9 @@
-//Parâmetros em funções
-function somar(n1, n2){//n1 e n2 são chamados como parâmetros
-    let resultado = n1 + n2;//resultado é a soma
-    console.log(resultado);
-}
-somar(1024, 742);//aqui meus números entram como parâmetros da função, entrando na operação
-
+//Retorno da função
+//ENTRADA - > PROCESSAMENTO - > SAÍDA
 function nomeCompleto(nome, sobrenome){
-    console.log(`Nome completo: ${nome} ${sobrenome}`);
+    return `${nome} ${sobrenome}`;//Retorno da função, será exibido no console.log(completo)
+    console.log("Testando 123");//Após o return, a função termina, então essa parte não é executada, seria apenas se estivesse acima do return
 }
-nomeCompleto("Jheane", "Silva")
+//let completo = nomeCompleto("Jheane", "Silva Santos");//Chama a função
+//console.log('Nome completo: ' + completo);//Imprime o retorno da função de forma expandida
+console.log('Nome completo: ' + nomeCompleto("Jheane", "Silva Santos"));//Imprime o retorno da função de forma compacta, já inserindo a chamada da função dentro do console.log
