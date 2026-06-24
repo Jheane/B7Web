db72f71049fd823166029ccf23c5ce0e55b251d4
Jheane <jheane.s.santos@gmail.com>
2026-06-24 16:40:50 -0300

    Arrow function

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,17 +1,16 @@
-//Exercício de funções #3
-/*Validar usuário e senha
-usuário correto: pedro
-senha correta: 123
-*/
-function validar(usuario, senha){
-    if(usuario === 'pedro' && senha === '123'){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-let usuario = 'pedro';
-senha = '1234';
-let validacao = validar(usuario, senha);
-console.log(validacao ? 'Acesso concedido!' : 'Acesso negado!');
+//Arrow function
+//forma convencional de usar funções:
+// function somar(x, y){
+//     return x + y;
+// }
+// console.log(somar(10,5));
+//forma reduzida:
+// const somar = (x, y) => {//Funções com mais de uma linha precisam ser escritas dessa forma
+//     let resultado = x + y;
+//     return resultado;
+// }
+const somar = (x, y) => x + y;//quando é uma linha apenas, posso escrever dessa forma, eliminando a escrita do return
+console.log(somar(10, 7));
+//Quando há apenas um parâmetro, consigo ainda eliminar os parênteses da função
+const sobrenome = sob => 'Jheane ' + sob;//sem parênteses, com adição de texto para melhor visualização
+console.log(sobrenome("Santos"));
