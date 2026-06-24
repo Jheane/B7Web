4d4366ae4783ebcbca8bd9109ca3556a95325047
Jheane <jheane.s.santos@gmail.com>
2026-06-24 16:20:54 -0300

    Exercício de funções #3

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,39 +1,17 @@
-//Exercício de funções #2
-/*Calcule o preço do imóvel
-- m2 = 3000.00
-- 1 quarto, m2x1
-- 2 quartos, m2x1.2
-- 3 quartos, m2x1.5
+//Exercício de funções #3
+/*Validar usuário e senha
+usuário correto: pedro
+senha correta: 123
 */
-// function calcularImovel (m, q){//Jeito que eu fiz
-//     if(q == 1){
-//         return (m * 3000);
-//     }
-//     else if(q == 2){
-//         return (m * (3000 * 1.2));
-//     }
-//     else{
-//         return (m * (3000 * 1.5));
-//     }
-// }
-function calcularImovel (metragem, quartos){//Forma que o professor fez
-    let m2 = 3000;
-    let preco = 0;
-    switch (quartos){
-        case 1:
-            default:
-                preco = metragem * m2;
-                break;
-        case 2:
-            preco = metragem * (m2 * 1.2);
-            break;
-        case 3:
-            preco = metragem * (m2 * 1.5);
-            break;
+function validar(usuario, senha){
+    if(usuario === 'pedro' && senha === '123'){
+        return true;
     }
-    return preco;
+    else{
+        return false;
+    }
 }
-let metragem = 123;
-let quartos = 1;
-let preco = calcularImovel(metragem, quartos);
-console.log(`A casa custa R$ ${preco}`);
+let usuario = 'pedro';
+senha = '1234';
+let validacao = validar(usuario, senha);
+console.log(validacao ? 'Acesso concedido!' : 'Acesso negado!');
