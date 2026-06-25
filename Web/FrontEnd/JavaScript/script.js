af5c020f5803aecf2ee03305ce24164627ff92d3
Jheane <jheane.s.santos@gmail.com>
2026-06-24 21:01:54 -0300

    Variáveis dentro de funções

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,16 +1,9 @@
-//Arrow function
-//forma convencional de usar funções:
-// function somar(x, y){
-//     return x + y;
-// }
-// console.log(somar(10,5));
-//forma reduzida:
-// const somar = (x, y) => {//Funções com mais de uma linha precisam ser escritas dessa forma
-//     let resultado = x + y;
-//     return resultado;
-// }
-const somar = (x, y) => x + y;//quando é uma linha apenas, posso escrever dessa forma, eliminando a escrita do return
-console.log(somar(10, 7));
-//Quando há apenas um parâmetro, consigo ainda eliminar os parênteses da função
-const sobrenome = sob => 'Jheane ' + sob;//sem parênteses, com adição de texto para melhor visualização
-console.log(sobrenome("Santos"));
+//variáveis dentro de funções
+function add(){
+    let count = 0;//variável local, tem preferência sobre a global, mesmo com o mesmo nome
+    count++;
+}
+let count = 0;//variável global
+add();
+add();
+console.log(count);
