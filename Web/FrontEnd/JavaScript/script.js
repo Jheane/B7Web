effe4fe1723cf0f38e3741956ce935d0d511a68f
Jheane <jheane.s.santos@gmail.com>
2026-06-25 17:07:51 -0300

    O que são objetos?

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,16 +1,21 @@
-//Exercício de array
-//1. No array abaixo, qual o número que pega a Ferrari?
-let carros = ['BMW', 'Ferrari', 'Mercedes'];
-let x = 1;//Com x = 1 e jogando ele na próxima linha, ele vai buscar o item de índice 1 no array
-console.log('1. ' + carros[x]);
-//2. Troque a Ferrari por Audi
-console.log(carros);
-carros[1] = 'Audi';
-console.log('2. Lista com Audi: ' + carros);
+//O que são objetos?
+//O objeto é parecido com o array, onde, dentro dele, há várias informações, porém funciona e é reconhecido de forma diferente do array pelo JS
+let nome = 'Jheane';
+let nomes = ['Bianca', 'Joanne', 'Jane', 'João'];
+let personagem = {//Declaração do objeto
+    nome: 'Jheane',//propriedade: informação (chave-valor)
+    idade: 31,
+    pais: 'Brasil',
+    caracteristicas: {//Objeto dentro de objeto
+        forca: 150,
+        magia: 220,
+        stamina: 4
+    },
+    olhos: ['preto', 'azul']//array dentro de objeto
+}
 
-//3. Adicione Volvo à lista
-carros.push('Volvo');//add Volvo à lista
-console.log('3. Lista com Volvo: ' + carros);
-
-//4. Exiba quantos itens tem no array
-console.log(`4. Itens no array: ${carros.length}`);
+console.log(typeof(nome), typeof(nomes), typeof(personagem));//aqui, tanto nomes quanto personagem é como objeto na saída do JS, mas o funcionamento é diferente
+console.log(personagem.idade);//acesso à propriedade para impressão do dado desejado
+console.log(personagem.caracteristicas.forca);//acesso em cascata, para ver a propriedade força dentro de caracteristicas, que está dentro de personagem
+console.log(personagem.olhos[1]);//consulta de item do array dentro do objeto
+//. para acessar característica do objeto, [] para acessar índice do array
