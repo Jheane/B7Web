f22726e652352bf21192460d16b2cc9669ab6594
Jheane <jheane.s.santos@gmail.com>
2026-06-28 15:01:20 -0300

    Iteração de array 1

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,27 +1,19 @@
-//Ordenação de array
-// let fruits = ['maçã','banana','kiwi','uva'];
-// console.log(fruits);
-// fruits.sort();//ordena os itens do array de a para z, incluindo o index de cada elemento
-// console.log(fruits);
-// fruits.reverse();//ordena os itens do array de z para a
-// console.log(fruits);
-let cars = [
-    {brand: 'Fiat', year: 2022},
-    {brand: 'BMW', year: 2018},
-    {brand: 'Ferrari', year: 2020}
-];
+//Iteração de array 1
+let fruits = ['maçã','banana','kiwi','uva','groselha','pera','laranja'];
+let bigFruits = fruits.filter((item) => {//a variável bigFruits recebe a função com filtro de frutas com mais de 4 letras
+    return item.length > 4;
+});
+console.log(bigFruits);
 
-// cars.sort((a, b) => {
-//     // if(a.year > b.year){
-//     //     return 1;
-//     // } else if(a.year < b.year){
-//     //     return -1;
-//     // } else{
-//     //     return 0;
-//     // }//essa forma foi para entendermos a lógica da situação, podendo ser simplificado conforme linha abaixo
-//     return a.year - b.year;
-// });//Ou ainda podemos fazer de forma mais resumida:
-cars.sort((a, b) => a.year - b.year);
-console.log(cars);
-cars.sort((a, b) => b.year - a.year);//Inverto para ordenar do mais novo para o mais antigo
-console.log(cars);
+let ok = fruits.every((value) => {//O every é como o E, onde irá retornar true caso todos os elementos entrem na condição
+    value.length > 3 ? true : false;
+});
+console.log(ok == true ? "Todos são maiores que 3" : "Não são todos maiores que 3");
+
+let ok1 = fruits.some((value) => {//Já o some é como o OU, onde irá retornar true caso um elemento seja satisfatório
+    return value.length > 4;
+});
+
+console.log(ok1 == true ? "Algum item é maior que 3" : "Nenhum item é maior que 3");
+
+console.log(fruits.includes('uva') ? "Tem uva" : "Não tem uva");//O includes pesquisa o item que passei dentro do array
