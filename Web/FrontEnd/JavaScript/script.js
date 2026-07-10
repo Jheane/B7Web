74fa3566d0cacf3ebabec178cc72015aa8a8480d
Jheane <jheane.s.santos@gmail.com>
2026-07-10 20:22:10 -0300

    Manipulando elementos 2

--- Web/FrontEnd/JavaScript/script.js
+++ Web/FrontEnd/JavaScript/script.js
@@ -1,20 +1,15 @@
-/*Manipulando elementos 1*/
+/*Manipulando elementos 2*/
 function clicou(){
     const teste = document.querySelector('#teste');
-    const teste2 = document.querySelector('#teste2');
-    console.log(teste);//retorna o array dentro da div #teste (inclui a estrutura da div, o ul, lis e o que mais incluir dentro dela)
-    console.log(teste.children); //retorna o filho imediato como HTMLCollection, em um array, aqui, no caso, sendo o ul e o strong (não traz o li porque não é filho imediato, é filho do filho)
-    console.log(teste.children[0].children); //aqui já retorna os filhos dos filhos, trazendo os elementos dentro da ul
-    const ul = teste.querySelector('ul');//seleciona o item ul
-    console.log(ul);
+    const ul = teste.querySelector('ul');
+    //métodos: funções próprias do elemento para manipulação
+    ul.children[0].append(" (alterado)");//o append adiciona conteúdo ao conteúdo existente no elemento, funcionando apenas para texto (não interpreta como html, como o inner). O append, diferente do innerHTML, concatena a inforamção ao conteúdo, sem alterar tudo. Já o inner pega aquele conteúdo, concatena e altera todo o conteúdo. Ou seja, ele some da memória do navegador e é preenchido por outro conteúdo com a alteração aplicada. Isso explica o porquê de, quando usamos o append com algum texto selecionado do conteúdo selecionado, ao clicar no botão, ele vai continuar selecionado após o clique no botão, diferente do inner, que vai remover a seleção por se tratar de outro item.
 
-    const ul2 = teste2.querySelector('ul');//seleciono novamente minha ul, para uma nova variável
+    let newLi = document.createElement("li");//cria um novo li, sem nada
+    newLi.innerText = "Item adicionado";//adiciona a informação na variável, mas não é mostrada na tela
 
-    console.log(ul.innerHTML); //mostra o conteúdo dentro do elemento, sendo retornado como texto. Posso usar para consultar e para alterar o valor
-
-    // ul.innerHTML = "<li>Item alterado</li>";//innerHTML: altera o conteúdo da minha ul para o que informei dentro das aspas, sendo descartada a alteração após atualizar a tela
-    // ul2.innerHTML += "<li>Item alterado</li>";//aqui, invés de substituir os itens da minha ul, ele vai aidcionar o que determinei nas aspas, juntando com o que já existe na ul
-    // ul2.children[0].innerHTML = "<li>Item do <strong>li[0]</strong> alterado</li>"; //altera o item do li, na posição 0, apenas. Ele aceita comandos HTML, diferente do innerText, que permite apenas texto.
-    console.log(ul.outerHTML);//aqui ele mostra o conteúdo do elemento ul, incluindo a parte de fora, que é o próprio ul em si (não pegando apenas o li dentro do elemento)
-    ul.outerHTML = "<strong>Alterado!</strong>"; //aqui ele altera o elemento, mas alterando tudo. Não é muito utilizado.
+    ul.appendChild(newLi);//agora ele aparece, e sem remover a seleção dos elementos anteriores. É o jeito mais otimizado de adicionar itens, visto que o inner pode modificar a questão de processamento para telas com muitos dados para carregar.
+    //O appendChild adiciona o item no final da listagem. Para adicionar no início, usamos o comando abaixo:
+    ul.prepend(newLi);
+    
 }
